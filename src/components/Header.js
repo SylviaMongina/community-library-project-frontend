3deda6502334ba3a4757e604c8ed0c551f0f0291
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-03-01 18:57:07 +0300

    coding

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const Header = ({ isDarkMode, onToggleDarkMode }) => {
+    const buttonTextContent = isDarkMode ? "Light Mode" : "Dark Mode";
+  
+    return (
+        <header className="navigation">
+          <h1 className="branding">
+            <Link to="/">
+              <span className="logo">{""}</span>
+              Community Library
+            </Link>
+          </h1>
+          <nav>
+            <NavLink className="button" exact to="/books">
+              All Books
+            </NavLink>
+            <NavLink className="button" to="/books/new">
+              Add Book
+            </NavLink>
+            <NavLink className="button" to="/about">
+              About
+            </NavLink>
+            <button onClick={onToggleDarkMode}>{buttonTextContent}</button>
+          </nav>
+        </header>
+      );
+    };
+    
+    export default Header;
