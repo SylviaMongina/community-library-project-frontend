3e5ce82a8981f68989f774cc1569aa555c9c604c
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-03-01 16:59:41 +0300

    coding

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section className="about">
+      <h1>ABOUT US</h1><br></br>
+    
+      <ul><br></br>
+        <li>This is a Community Library everyone is welcomed</li>
+        <li>Support a needy child by donating a book</li>
+        <li>Support your best author by getting a copy</li>
+
+      </ul>
+    </section>
+  )
+}
+
+export default About
