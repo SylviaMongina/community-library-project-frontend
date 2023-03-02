6c2ca07c610ef4181cb6945ee69505f3c03da2fa
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-03-02 12:01:14 +0300

    correcting and updating

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,12 @@ import React from 'react'
 function About() {
   return (
     <section className="about">
-      <h1>ABOUT US</h1><br></br>
+      <h1>ABOUT</h1><br></br>
     
       <ul><br></br>
-        <li>This is a Community Library everyone is welcomed</li>
-        <li>Support a needy child by donating a book</li>
-        <li>Support your best author by getting a copy</li>
+        <li>Free Library Books Online</li>
+        <li>Keep Track of your Favourite Books</li>
+        <li>Try Fulltext Search</li>
 
       </ul>
     </section>
